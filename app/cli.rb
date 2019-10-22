66a996dd72c0770fa3988065ebd260bb6b0ad6fb
github username <github email address>
2019-10-22 15:16:41 -0600

    adds functions to cli

--- app/cli.rb
+++ app/cli.rb
@@ -5,7 +5,30 @@ class Cli
         @prompt = TTY::Prompt.new
     end
 
+    def welcome_page
+        puts "Welcome to the Tate Museum Arts App"
+    end
 
+    def prompt_for_new_or_returning_user
+        prompt.select("Please Select:", ("New User","Returning User"))
+    end
+
+    def prompt_for__new_user
+        prompt.ask('What is your name:')
+    end
+
+    def prompt_for_returning_user 
+        names = User.pluck(:name)
+        prompt.select("Please select your name", names)
+    end
 
     
+
+
+
+
+
+
+
+
 end
