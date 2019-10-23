17417d87b00ae276a1928ef3fe99c83baafc1e64
diana-mm <dmmiller85@gmail.com>
2019-10-23 15:03:31 -0600

    adds your favorites functionality to main menu

--- runner.rb
+++ runner.rb
@@ -3,53 +3,48 @@ require_relative './config/environment.rb'
 def start
 
   @@selected_artist = nil
+  @@selected_artwork = nil
+  
+  @@cli = Cli.new
+ 
+  @@current_user = new_or_returning
+  
+    @@cli.welcome_page
 
-    cli = Cli.new
-     
-    cli.welcome_page
 
-    current_user = new_or_returning(cli)
-
-    main_menu(cli)
-
-    artists_artwork(cli)
+    main_menu
 
 
 end  
 
-def new_or_returning(cli)
-  user = cli.prompt_for_new_or_returning_user
+def new_or_returning
+  user = @@cli.prompt_for_new_or_returning_user
   if user == "New"
-    new_user = cli.prompt_for_new_user
-    current_user = User.create(name: new_user)
+    new_user = @@cli.prompt_for_new_user
+    @@current_user = User.create(name: new_user)
   else
-    returning_user = cli.prompt_for_returning_user
-    current_user = User.find_by(name: returning_user)
+    returning_user = @@cli.prompt_for_returning_user
+    @@current_user = User.find_by(name: returning_user)
   end
 end
 
-def main_menu(cli)
-    selection = cli.main_menu_prompt
+def main_menu
+    selection = @@cli.main_menu_prompt
     case selection
-    when 'Browse Artwork'
-        @@selected_artist = cli.browse_by_artist(Artist.all)
     when 'View Favorites'
-        puts 'Your Favorites'
-        cli.view_favorites(Favorites.all)
+      puts 'Your Favorites'
+      @@cli.view_favorites(@@current_user)
+      main_menu
+    when 'Browse Artwork'
+        @@selected_artist = @@cli.browse_by_artist(Artist.all)
+        @@cli.artwork_by_artist(@@selected_artist)
+        favoriting_artwork
     when 'Recommend Artwork'
         puts 'Here are some recommendations:'
-        cli.recommend_artwork(Artwork.all.sample(5))
+        @@cli.recommend_artwork(Artwork.all.sample(5))
     end
+  end
 
-    def artists_artwork(cli)
-      cli.artwork_by_artist(@@selected_artist)
-    end
-
-    
-
-end
-      # binding.pry
-
-
+   
 
 start 
